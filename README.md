7f5fa155b3e80d63f8f55663685e46612e2add73
cangirhabil <hblcngr1400@gmail.com>
2025-09-02 15:26:00 +0200

    Add initial project structure and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DocMind
 RAG Based Knowladge Managment System
+
+
+#1
+conda env list
+#2
+conda create -n medicalchatbot python=3.10 -y
+#3
+conda activate medicalchatbot
+#4
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MedicalChatBot",
+    version="0.1.0",
+    author="Habil Cangir",
+    author_email="habilcangir@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
