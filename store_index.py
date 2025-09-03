161e09be627a078a9c77d1f010b371d4fd75b680
cangirhabil <hblcngr1400@gmail.com>
2025-09-03 13:31:56 +0200

    Implement initial setup for Medical Chatbot with PDF loading, text splitting, and embedding functionality; add chat interface and styling.

---
+++ store_index.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+import os
+from pinecone import Pinecone, ServerlessSpec
+from src.helper import load_pdf_files, text_split, download_embeddings, filter_to_minimal_docs
+from langchain_pinecone.vectorstores import PineconeVectorStore
+
+load_dotenv()
+
+
+index_name = "medical-chatbot"
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+extracted_data = load_pdf_files("data/")
+minimal_docs = filter_to_minimal_docs(extracted_data)
+texts_chunk = text_split(minimal_docs)
+
+embedding = download_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+
+
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name, 
+        dimension=384, 
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+index = pc.Index(index_name)
+
+
+doc_search = PineconeVectorStore.from_documents(
+    documents=texts_chunk,
+    embedding=embedding,
+    index_name=index_name
+)
+
